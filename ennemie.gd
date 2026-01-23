68c07b5020b9408432c437eb2d79bc64cc04b37f
Starg0ne <boubame@protonmail.com>
2026-01-23 11:45:50 +0100

    Update ennemie.gd
    Ajout commentaire

--- ennemie.gd
+++ ennemie.gd
@@ -1,16 +1,14 @@
 extends CharacterBody2D
 
-
-# On crée des variables réglables dans l'inspecteur
-@export var speed = 250.0
+@export var speed = 250.0 # Vitesse de l'ennemie
 var direction = 1  # 1 = droite, -1 = gauche
 
 func _physics_process(delta):
-	velocity.x = direction * speed * delta
+	velocity.x = direction * speed * delta 
 	
-	move_and_collide(velocity)
+	move_and_collide(velocity) #Déplacement
 	
-	if (position.x > 1000):
-		direction *= -1
-	elif (position.x < 0):
-		direction *= -1
+	if (position.x > 1000): # Borne Droite
+		direction = -1
+	elif (position.x < 0): # Borne Gauche
+		direction = 1
