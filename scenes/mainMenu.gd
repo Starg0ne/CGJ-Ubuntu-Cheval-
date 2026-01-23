e51e8e901f076946d7c26331f2f3954f634eadc4
Firscroll <lucasmichel2100@gmail.com>
2026-01-23 15:35:38 +0100

    Création de la partie logique du menu pause + création du main menu
    J'ai créé le menu pause qui maintenant entièrement fonctionnel, et le main menu entièrement fonctionnellement.

---
+++ scenes/mainMenu.gd
@@ -0,0 +1,22 @@
+extends Panel
+#Variables
+@onready var commencer = $ButtonCommencer
+@onready var quitter = $ButtonQuitter
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	#Quand le bouton commencer est appuyé, on va aller sur la scéne level
+	if (commencer.button_pressed):
+		get_tree().change_scene_to_file("res://scenes/level.tscn")
+	
+	#Quand le bouton quitter est appuyé, on va quitter le jeu
+	if (quitter.button_pressed):
+		get_tree().quit(0)
+	
+	pass
